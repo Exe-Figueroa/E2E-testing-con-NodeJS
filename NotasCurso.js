b56b0920084d458c04f6f2490d2afa665e4243f8
FacundoExequielFigueroa <facundofigueroa789@gmail.com>
2024-01-12 11:24:29 -0300

    04- Instalando y configurando jest con nodejs

--- NotasCurso.js
+++ NotasCurso.js
@@ -4,3 +4,11 @@
  * *Se recorre la API desde insomnia
  *  ?Creación de usuario, categorías y productos
  */
+
+/** 03-Instalando Jest
+ * *Instalar jest
+ * *Configurar package.json con el script de jest
+ * *Agregar archivo de configuración de jest en la raiz
+ * *Crear carpeta e2e para guardar los test ahí
+ * *Se le agrega un comando --verbose para que los test sean más explícitos  
+ */
---
+++ e2e/sum.e2e.js
@@ -0,0 +1,6 @@
+describe('First test', () => {
+  
+  test('3 + 3 = 6', () => {
+    expect(3 + 3).toBe(6);
+  });
+});
