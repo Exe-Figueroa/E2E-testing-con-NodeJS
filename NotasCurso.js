5082b4b6a95386b8e817c0473684198c02cf91b8
FacundoExequielFigueroa <facundofigueroa789@gmail.com>
2024-01-12 10:58:10 -0300

    02-Recorriendo la API

---
+++ NotasCurso.js
@@ -0,0 +1,6 @@
+/** 02-Explorando la API
+ * *Se corren migraciones
+ * *Se configuran los contenedores de docker
+ * *Se recorre la API desde insomnia
+ *  ?Creación de usuario, categorías y productos
+ */
